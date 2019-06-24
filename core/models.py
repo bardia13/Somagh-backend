946d1f9b4b76629ebbfe1f961e08c976f7547877
bardia13 <bardia.abhari13@gmail.com>
2019-06-24 21:04:06 +0430

    added few models

--- core/models.py
+++ core/models.py
@@ -23,3 +23,42 @@ class Profile(models.Model):
     nationalId = models.CharField(max_length=10, null=True, blank=True)
     userRole = models.CharField(max_length=1, choices=userRoleChoices, default=Manager)
     verified = models.BooleanField(default=False)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    manager = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='department_manager')
+    members = models.ManyToManyField(User, related_name='department')
+
+
+class Case(models.Model):
+    Complaint = 'C'
+    Request = 'R'
+    Critique = 'Q'
+    Suggestion = 'S'
+    typeChoices = (
+        (Complaint, 'شکایت'),
+        (Request, 'درخواست'),
+        (Critique, 'انتقاد'),
+        (Suggestion, 'پیشنهاد')
+    )
+    Open = 'O'
+    Closed = 'C'
+    InQueue = 'I'
+    Postponed = 'P'
+    statusChoices = (
+        (Open, 'باز'),
+        (Closed, 'بسته'),
+        (InQueue, 'در انتظار'),
+        (Postponed, 'به تعویق افتاده')
+    )
+    creator = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='cases')
+    department = models.ForeignKey(Department, on_delete=models.DO_NOTHING, related_name='cases')
+    title = models.CharField(max_length=200, null=False)
+    type = models.CharField(max_length=1, choices=typeChoices, default=Request)
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(null=False)
+    status = models.CharField(max_length=1, choices=statusChoices, default=Open)
+
+
+# class Refer(models.Model)

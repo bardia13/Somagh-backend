3b782e5ebeb779cd18ab43d83e36b2eb0401b15e
bardia13 <bardia.abhari13@gmail.com>
2019-06-25 00:31:48 +0430

    added user management apis

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
-from .views.user import UserRegisterView, CurrentProfileDetail
+from .views.user import UserRegisterView, CurrentProfileDetail, RetrieveProfile, ConfitmProfile, DeactiveProfile,\
+    DeleteProfile
 from .views.case import CaseCreate, ReferList, Action, CaseList, CaseChangeSatisfied
 
 urlpatterns = [
     path('user/register/', UserRegisterView.as_view()),
     path('user/detail/', CurrentProfileDetail.as_view()),
+    path('user/detail/<int:pk>/', RetrieveProfile.as_view()),
+    path('user/confirm/<int:pk>/', ConfitmProfile.as_view()),
+    path('user/deactive/<int:pk>/', DeactiveProfile.as_view()),
+    path('user/delete/<int:pk>/', DeleteProfile.as_view()),
     path('case/create/', CaseCreate.as_view()),
     path('case/list/', CaseList.as_view()),
     path('case/list/satisfied/', CaseChangeSatisfied.as_view()),
--- core/views/user.py
+++ core/views/user.py
@@ -1,6 +1,9 @@
-from rest_framework.generics import CreateAPIView, RetrieveAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView, DestroyAPIView, GenericAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from ..models import Profile
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework import status
 from django.contrib.auth.models import User
 from ..serializers import RegisterUserSerialzier, ProfileDetailSerializer
 
@@ -16,3 +19,50 @@ class CurrentProfileDetail(RetrieveAPIView):
 class UserRegisterView(CreateAPIView):
     permission_classes = []
     serializer_class = RegisterUserSerialzier
+
+
+class ProfileList(ListAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    serializer_class = ProfileDetailSerializer
+    queryset = Profile.objects.all()
+
+
+class RetrieveProfile(RetrieveAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    serializer_class = ProfileDetailSerializer
+
+    def get_object(self, pk):
+        try:
+            return Profile.objects.get(user_id=pk)
+        except Profile.DoesNotExist:
+            raise Http404
+
+
+class DeleteProfile(DestroyAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    serializer_class = ProfileDetailSerializer
+
+    def get_object(self, pk):
+        try:
+            return Profile.objects.get(user_id=pk)
+        except Profile.DoesNotExist:
+            raise Http404
+
+
+class DeactiveProfile(GenericAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get(self, request, pk):
+        profile = Profile.objects.get(user_id=pk)
+        profile.user.is_active = False
+        return Response({"message" : "done"}, status=status.HTTP_200_OK)
+
+
+class ConfitmProfile(GenericAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get(self, request, pk):
+        profile = Profile.objects.get(user_id=pk)
+        profile.user.is_active = True
+        return Response({"message" : "done"}, status=status.HTTP_200_OK)
+
